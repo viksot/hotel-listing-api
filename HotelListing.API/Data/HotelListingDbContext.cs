1cc6449fdb23fab08e1631d33a965d6bbde09817
Viktor Sotsuk <sotsukv@gmail.com>
2023-03-07 19:55:17 +0400

    Added entity classes Country.cs and Hotel.cs in Data folder. Сorresponding DbSets added in DbContext. InitialMigration made

--- HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -8,5 +8,9 @@ namespace HotelListing.API.Data
         {
                 
         }
+
+        public DbSet<Hotel> Hotels { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
     }
 }
