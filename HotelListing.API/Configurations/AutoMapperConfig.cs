f7f45cf01e19d297646008c97ec9a1b886190b43
Viktor Sotsuk <sotsukv@gmail.com>
2023-03-08 20:56:22 +0400

    Added AutoMapper service in Program.cs. Added AutoMapperConfig.cs in Configurations folder

---
+++ HotelListing.API/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using HotelListing.API.Data;
+using HotelListing.API.Models.Country;
+using HotelListing.API.Models.Hotel;
+
+namespace HotelListing.API.Configurations
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Country, CountryToCreateDto>().ReverseMap();
+            CreateMap<Country, CountryToGetDto>().ReverseMap();
+            CreateMap<Country, CountryDto>().ReverseMap();
+            CreateMap<Country, CountryToUpdateDto>().ReverseMap();
+
+            CreateMap<Hotel, HotelDto>().ReverseMap();
+        }
+    }
+}
+ 
--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,3 +1,4 @@
+using HotelListing.API.Configurations;
 using HotelListing.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
@@ -27,6 +28,8 @@ builder.Services.AddCors(options =>
 
 builder.Host.UseSerilog((ctx, lc) =>
     lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+builder.Services.AddAutoMapper(typeof(AutoMapperConfig));
 
 var app = builder.Build();
 
